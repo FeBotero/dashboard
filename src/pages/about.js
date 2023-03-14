70ea8c83382b8a2ede9bd336c66e21b3e15c423b
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-03-14 13:30:56 -0400

    Project Dashboard

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,11 @@
+import Navbar from "@/components/Navibar";
+import Link from "next/link";
+
 export default function About() {
-  return <h1>Pagina de About</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>Pagina de About</h1>
+    </>
+  );
 }
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,3 +1,11 @@
+import Navbar from "@/components/Navibar";
+import Link from "next/link";
+
 export default function Products() {
-  return <h1>Página de Produtos</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>Página de Produtos</h1>
+    </>
+  );
 }
